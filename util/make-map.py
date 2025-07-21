863ebd56ef943d13a91f6b3cab06d63eae750c15
John Aegard <johnzo@johnzo.com>
2025-07-21 01:06:18 -0700

    cool blobby maps

--- util/make-map.py
+++ util/make-map.py
@@ -3,20 +3,38 @@
 import random
 
 def random_petscii():
+
+  h = 256
+  w = 128
+
   b = bytearray()
   b += bytes([2,3])
 
-  colors=[0,11,9]
+  colors=[6,9,9,9,9,9,11,11,11,11,11,12,12,12,12,12,15]
+
+  color_grid = []
+  color_grid = [[0 for _ in range(w)] for _ in range(h)]
+  
+  for row in range(h):
+    for col in range(w):
+      color_grid[row][col] = colors[random.randint(0,len(colors)-1)]
+
+  for row in range(1,h):
+    for col in range(1,w):
+      roll = random.randint(0,99) 
+      if roll < 45: 
+        color_grid[row][col] = color_grid[row][col-1]
+      elif roll < 90: 
+        color_grid[row][col] = color_grid[row-1][col]
+      else:
+        color_grid[row][col] = colors[random.randint(0,len(colors)-1)]
 
   for row in range(256):
     for col in range(128):
-      # tile = 0 if (row < 4 or row > 251 or col < 4 or col > 123) else colors[random.randint(0,len(colors)-1)] 
-      # tile = 0 if (row < 4 or row > 251 or col < 4 or col > 123) else int(row / 16)  
-      # b += bytes([tile,0])
       tile = random.randint(0x40,0x7f)
-      color = ((int(row/16) + int(col/16)) % 15 + 1);
+      color = color_grid[row][col]
       b += bytes([tile,color])
-  return b    
+  return b
 
 b = random_petscii()
 filename="map0.bin"
@@ -24,6 +42,9 @@ with open(filename, 'wb') as f:
   f.write(b)
   
 print(f"Wrote {len(b)} bytes to {filename}")
+
+
+
 
 b1=bytearray()
 b1 += bytes([2,3])
@@ -54,10 +75,8 @@ for row in range(32):
     b1 += bytes([char,color])
 
 wind_label_addr = 2 + ( (23*64) + 35) *2;
-print(len(b1));
 color = 0x21;
 b1[wind_label_addr:wind_label_addr+8] = bytes([0x17, color, 0x09, color, 0x0e, color, 0x04, color])
-print(len(b1));
 
 filename1="map1.bin"
 with open(filename1, 'wb') as f:
